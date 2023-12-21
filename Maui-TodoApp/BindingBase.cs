2491a1fffefa0255b7fe85ee7130bd1cff54ee85
ino <87593133+inoUwU@users.noreply.github.com>
2023-12-22 00:02:23 +0900

    BindableBase実装

---
+++ Maui-TodoApp/BindingBase.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Maui_TodoApp
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (Equals(field, value)) { return false; }
+            field = value;
+            //プロパティ変更を通知
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); return true;
+        }
+    }
+}
