e12c4792ae4a31c1258a37fa4018f5aae07f01ac
ino <87593133+inoUwU@users.noreply.github.com>
2023-12-24 01:27:13 +0900

    Windows版のwindowsizeを変更

--- Maui-TodoApp/Platforms/Windows/App.xaml.cs
+++ Maui-TodoApp/Platforms/Windows/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Windows.Graphics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -17,6 +20,21 @@ namespace Maui_TodoApp.WinUI
         public App()
         {
             this.InitializeComponent();
+
+            int width = 600;
+            int height = 600;
+
+            Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+            {
+                var mauiWindow = handler.VirtualView;
+                var nativeWindow = handler.PlatformView;
+                nativeWindow.Activate();
+                IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+                WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+                AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+                appWindow.Resize(new SizeInt32(width, height));
+
+            });
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
